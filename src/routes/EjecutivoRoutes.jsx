5b54ba60e1ce677fed69b126abc21f3831fe8a7a
Carlos Javier Diaz Flores <carlosdiaz.dev@gmail.com>
2022-04-27 01:17:28 -0500

    Several changes on view Cliente and Beneficiario UI

--- src/routes/EjecutivoRoutes.jsx
+++ src/routes/EjecutivoRoutes.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import TabsCuentas from '../components/ejecutivo/TabsCuentas'
-import CrearCuenta from '../components/ejecutivo/CrearCuenta'
-import FormularioBeneficiario from '../components/ejecutivo/FormularioBeneficiario'
-import Home from '../components/ejecutivo/Home'
-import LayoutEjecutivo from '../components/ejecutivo/LayoutEjecutivo'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TabsCuentas from "../components/ejecutivo/TabsCuentas";
+import CrearCuenta from "../components/ejecutivo/CrearCuenta";
+import FormularioBeneficiario from "../components/ejecutivo/FormularioBeneficiario";
+import Home from "../components/ejecutivo/Home";
+import LayoutEjecutivo from "../components/ejecutivo/LayoutEjecutivo";
 
-const EjecutivoRoutes = ({setLoginSuccess, setRole}) => {
+const EjecutivoRoutes = ({ setLoginSuccess, setRole }) => {
   return (
-     <BrowserRouter>
-     <Routes>
-         <Route path='/' element={<LayoutEjecutivo setLoginSuccess={setLoginSuccess} 
-                                                setRole={setRole}/>}>
-             <Route index element={<Home />} />
-             <Route path='crear-cliente' element={<TabsCuentas/>}/>
-             <Route path="formulario-beneficiario" element={<FormularioBeneficiario />}/>
-             <Route path="crear-cuenta" element={<CrearCuenta />}/>
-         </Route>
-     </Routes>
-  </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LayoutEjecutivo
+              setLoginSuccess={setLoginSuccess}
+              setRole={setRole}
+            />
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="crear-cliente" element={<TabsCuentas />} />
+          <Route
+            path="formulario-beneficiario"
+            element={<FormularioBeneficiario />}
+          />
+          <Route path="crear-cuenta" element={<CrearCuenta />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default EjecutivoRoutes
+export default EjecutivoRoutes;
