c7550e1d9e70cb1cfb6c39d04ab87485d0c3006d
Carlos Javier Diaz Flores <carlosdiaz.dev@gmail.com>
2022-04-29 11:37:31 -0500

    ADD changes un UI en Gerente View

--- src/components/ejecutivo/CrearCuenta.jsx
+++ src/components/ejecutivo/CrearCuenta.jsx
@@ -1,83 +1,108 @@
-import React from 'react';
+import React from "react";
 import { useFormik } from "formik";
-import {useState} from 'react';
-import service from '../../service';
+import { useState } from "react";
+import service from "../../service";
 import * as Yup from "yup";
 import {
-    Card,
-    CardActions,
-    CardContent,
-    CardMedia,
-    Button,
-    Typography,
-    Container,
-    Grid
-  } from "@mui/material";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CreditCardIcon from '@material-ui/icons/CreditCard';
-import { Link } from 'react-router-dom'
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Container,
+  Grid,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import { Link } from "react-router-dom";
 
 const theme = createTheme({
-    palette:{
-        primary:{
-            main: '#01579b',
-        }
+  palette: {
+    primary: {
+      main: "#01579b",
     },
+  },
 });
 
 const CrearCuenta = () => {
-
-
   return (
-    <Container component="main" maxWidth="sm" sx={{ mb: 8 }}>  
-        <Grid container spacing={5}>
-            <h2>Seleccione el tipo de cuenta</h2>  
-            <Grid item xs={12} sm={6}>
-                <Card sx={{ maxWidth: 300, cursor: 'pointer' }}>
-                  <Link to="/crear-cliente/debito" state={{type: 'Debito'}}>
-                    <CardMedia
-                    component="img"
-                    height="200"
-                    image="https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h4" component="div">
-                                Débito
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                    {/* <ThemeProvider theme={theme}> */}
-                            {/* <Button size="medium" color="primary" startIcon={<CreditCardIcon/>}>Crear</Button> */}
-                        {/* </ThemeProvider> */}
-                    </CardActions>
-                    </Link>
-                </Card>
-            </Grid>
-            <br></br>
-            <Grid item xs={12} sm={6}>
-                    <Link to="/crear-cliente/credito"  state={{type: 'Credito'}}>
-                <Card sx={{ maxWidth: 300, cursor: 'pointer' }}>
-                    <CardMedia
-                    component="img"
-                    height="200"
-                    image="https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h4" component="div">
-                            Crédito
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                    {/* <ThemeProvider theme={theme}> */}
-                            {/* <Button size="medium" color="primary" startIcon={<CreditCardIcon/>}>Crear</Button> */}
-                    {/* </ThemeProvider> */}
-                    </CardActions>
-                </Card>
-                        </Link>
-            </Grid>
+    <Container component="main" maxWidth="sm" sx={{ mb: 8 }}>
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={12} sx={{textAlign:'center', marginBottom:'3rem'}}>
+          <Typography variant="h4" color="initial">
+            Seleccione el tipo de cuenta
+          </Typography>
         </Grid>
-    </Container>
-  )
-}
 
-export default CrearCuenta
+        <Grid item xs={12} sm={6}>
+          <Card sx={{ maxWidth: 300, cursor: "pointer", textAlign: "center" }}>
+            <Link
+              to="/crear-cliente/debito"
+              state={{ type: "Debito" }}
+              style={{ textDecoration: "none" }}
+            >
+              <CardMedia
+                component="img"
+                height="200"
+                image="https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h4"
+                  component="div"
+                  sx={{
+                    mt: 2,
+                    color: "rgba(0, 0, 0, 0.87)",
+                    textDecoration: "inherit",
+                    fontWeight: "bold",
+                }}
+                >
+                  Débito
+                </Typography>
+              </CardContent>
+            </Link>
+          </Card>
+        </Grid>
+        <br></br>
+
+        <Grid item xs={12} sm={6}>
+          <Link
+            to="/crear-cliente/credito"
+            state={{ type: "Credito" }}
+            style={{ textDecoration: "none" }}
+            >
+              
+            <Card
+              sx={{ maxWidth: 300, cursor: "pointer", textAlign: "center"}}
+            >
+              <CardMedia
+                component="img"
+                height="200"
+                image="https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h4"
+                  component="div"
+                  sx={{
+                    mt: 2,
+                    color: "rgba(0, 0, 0, 0.87)",
+                    textDecoration: "inherit",
+                    fontWeight: "bold"
+                  }}
+                >
+                  Crédito
+                </Typography>
+              </CardContent>
+            </Card>
+          </Link>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default CrearCuenta;
--- src/components/gerente/ListaCreditos.jsx
+++ src/components/gerente/ListaCreditos.jsx
@@ -1,78 +1,140 @@
-import { Button, Grid, InputLabel, Select, MenuItem } from '@material-ui/core'
-import {useState, useEffect} from 'react'
-import service from '../../service'
-import { useNavigate } from 'react-router-dom'
+import {
+  Button,
+  Grid,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl,
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import service from "../../service";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const ListaCreditos = () => {
+  const [credits, setCredits] = useState([]);
+  const [filter, setFilter] = useState("Todo");
+  const navigateTo = useNavigate();
+  // Hook de Notistack
+  const { enqueueSnackbar } = useSnackbar();
 
-     const [ credits, setCredits ] = useState([]);
-     const [ filter, setFilter ] = useState('Todo');
-     const navigateTo = useNavigate();
+  const getCredits = async () => {
+    const { developURL } = service;
+    const url = `${developURL}/credit/status/${filter}`;
+    const fetchConfig = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("t"),
+      },
+    };
 
-     const getCredits = async () => {
-          const { developURL } = service;
-          const url = `${developURL}/credit/status/${filter}`;
-          const fetchConfig = {
-               method: 'GET', 
-               headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem('t')} ,
-          }
+    try {
+      const response = await fetch(url, fetchConfig);
+      const responseJSON = await response.json();
 
-          try {
-               const response = await fetch(url, fetchConfig);
-               const responseJSON = await response.json();
+      if (!responseJSON) {
+        //Alerta no se hizo correctamente la peticion
+        enqueueSnackbar("Se ha cambiado tu contrseña satisfactoriamente", {
+          preventDuplicate: true,
+          variant: "success",
+        });
+        return;
+      }
 
-               if (!responseJSON) {
-                    //Alerta no se hizo correctamente la peticion
-                    return;
-               }
+      //Todo bien
+      enqueueSnackbar(responseJSON.msg, {
+        preventDuplicate: true,
+        variant: "success",
+      });
+      setCredits(responseJSON.result);
+    } catch (error) {
+      //alerta algo salio mal
+      enqueueSnackbar("Hubo un error intentalo de nuevo", {
+        preventDuplicate: true,
+        variant: "warning",
+      });
+    }
+  };
+  useEffect(() => {
+    getCredits();
+  }, [filter]);
 
-               //Todo bien
-               setCredits(responseJSON.result);
+  return (
+    <Box sx={{ width: "85%", margin: "auto" }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Box
+            sx={{
+              paddingBottom: "1rem",
+              borderBottom: 1,
+              borderColor: "divider",
+            }}
+          >
+            <Typography variant="h5" color="initial" sx={{ fontWeight: "600" }}>
+              Creditos a Aprobar
+            </Typography>
+          </Box>
+        </Grid>
 
-          } catch (error) {
-               //alerta algo salio mal
-          }
-     }
+        <Grid item xs={12}>
+          <FormControl sx={{ m: 1, width: "300px" }}>
+            <InputLabel id="selectFiltro">Filtro de Búsqueda</InputLabel>
+            <Select
+              labelId="selectFiltro"
+              id="status"
+              name="status"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            >
+              <MenuItem value={"Todo"}>Todo</MenuItem>
+              <MenuItem value={"Aprobado"}>Aprobado</MenuItem>
+              <MenuItem value={"Rechazado"}>Rechazado</MenuItem>
+              <MenuItem value={"Pendiente"}>Pendiente</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
 
-     useEffect(() => {
-          getCredits();
-     }, [filter]);
+        <Grid container spacing={3} sx={{ width: "90%", margin: "auto", marginTop:'3rem' }}>
+          {credits &&
+            credits.length > 0 &&
+            credits.map((c) => (
+              <Grid item xs={12} md={4} key={c.id}>
+                <Card>
+                  <CardContent sx={{ textAlign: "center" }}>
+                    <Typography variant="h5" component="div">
+                      {c.status}
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                      <b>Monto Solicitado:</b>  {`$ ${c.requestedAmount.toLocaleString('en-US', {maximumFractionDigits:2})}`}
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                    {/* <b>Fecha de Solicitud:</b>{c.requestedDate} {new Date(c.requestedDate).toLocaleDateString('en-US')} */}
+                      <b>Fecha de Solicitud:</b> {c.applicationDate}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      onClick={() => navigateTo(`/creditos/${c.id}`)}
+                      sx={{ margin: "auto", marginBottom: "1rem" }}
+                    >
+                      Ver Información
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-     return (
-       <div>
-            <Grid container>
-                 <Grid container item xs={12} >
-                    <InputLabel id="selectFiltro">Filtro de Búsqueda</InputLabel>
-                         <Select
-                           labelId="selectFiltro"
-                           id="status"
-                           name='status'
-                           value={filter}
-                           onChange={ (e) => setFilter(e.target.value) }
-                         >
-                         <MenuItem value={'Todo'}>Todo</MenuItem>
-                         <MenuItem value={'Aprobado'}>Aprobado</MenuItem>
-                         <MenuItem value={'Rechazado'}>Rechazado</MenuItem>
-                         <MenuItem value={'Pendiente'}>Pendiente</MenuItem>
-                    </Select>
-                 </Grid>
-               {
-                    credits && credits.length > 0 && (
-                         credits.map( c => (
-                              <Grid item xs={12} md={4} key={c.id}>
-                                   <p>{c.status}</p>
-                                   <Button color='primary' 
-                                           onClick={() => navigateTo(`/creditos/${c.id}`)}
-                                   >
-                                        Info
-                                   </Button>
-                              </Grid>
-                         ) )
-                    )
-               }
-            </Grid>
-       </div>
-     )
-}    
-
-export default ListaCreditos
+export default ListaCreditos;
